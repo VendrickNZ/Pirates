e1ff863942fef3089e6f9ce2e5d51231a1e2483f
Jakib <jakibisherwood88@gmail.com>
2025-08-31 00:25:32 +1200

    Add validation for game setup commands

--- src/game/GameSetup.ts
+++ src/game/GameSetup.ts
@@ -1,30 +1,63 @@
 import { stdin, stdout } from "node:process";
-import { createInterface } from "node:readline/promises";
+import { createInterface, Interface } from "node:readline/promises";
 import Player from "../models/Player";
 import GameManager from "../models/GameManager";
-import { printInformation } from "../utils/TextUtils";
+import { isValidGameDuration, isValidPlayerName, isValidWorldSeed, printInformation } from "../utils/TextUtils";
 
 export async function createNewGame() {
     const rl = createInterface({
         input: stdin,
         output: stdout,
     });
-
-    const name = await rl.question('Enter player name (3-15 characters, letters and spaces only): ');
-    const duration = await rl.question('How many days do you want the game to last (20-50)?: ')
-    const seed = await rl.question('Enter a world seed (optional): ');
+    const name = await promptPlayerName(rl);
+    const duration = await promptGameDuration(rl);
+    const worldSeed = await promptWorldSeed(rl);
     rl.close();
 
     printInformation('Intro text...')
 
     const player = createPlayer(name);
 
-    const gm = new GameManager(parseInt(duration), parseInt(seed), player);
+    const gm = new GameManager(duration, worldSeed, player);
     gm.printAvailableCommands();
 }
 
 function createPlayer(name: string): Player {
 
+
     const player = new Player(name);
     return player;
+}
+
+async function promptPlayerName(rl: Interface): Promise<string> {
+    const name = await rl.question('Enter player name (3-15 characters, letters and spaces only): ');
+
+    if (!isValidPlayerName(name)) {
+        rl.write(`${name} is invalid, please try again. \n`)
+        return promptPlayerName(rl);
+    } else {
+        return name;
+    }
+}
+
+async function promptGameDuration(rl: Interface): Promise<number> {
+    const duration = await rl.question('How many days do you want the game to last (20-50)?: ');
+
+    if (!isValidGameDuration(duration)) {
+        rl.write(`${duration} is invalid, please try again. \n`)
+        return promptGameDuration(rl);
+    } else {
+        return parseInt(duration);
+    }
+}
+
+async function promptWorldSeed(rl: Interface): Promise<number> {
+    const seed = await rl.question('Enter a world seed (optional): ');
+
+    if (!isValidWorldSeed(seed)) {
+        rl.write(`${seed} is invalid, please try again. \n`)
+        return promptWorldSeed(rl);
+    } else {
+        return parseInt(seed);
+    }
 }
--- src/utils/TextUtils.ts
+++ src/utils/TextUtils.ts
@@ -1,5 +1,8 @@
 import type { CompleterResult } from "readline";
 
+const MIN_GAME_DURATION = 20;
+const MAX_GAME_DURATION = 50;
+
 export function completer(line: string): CompleterResult {
     const completions = ['View Ship', 'View Cargo', 'Visit Dock', 'View Vendor', 'Hire Crew', 'Exit'];
     const formattedLine = formatCommand(line);
@@ -27,4 +30,29 @@ export function formatCommand(command: string): string {
 
 export function TimeoutInSeconds(seconds: number): Promise<NodeJS.Timeout> {
     return new Promise(resolve => setTimeout(resolve, seconds * 1000)) // timeout uses milliseconds
+}
+
+export function isValidPlayerName(name: string): boolean {
+    const regex = new RegExp('^[a-zA-Z ]{3,15}$');
+    return regex.test(name);
+}
+
+export function isValidGameDuration(durationString: string): boolean {
+    if (isNumber(durationString)) {
+        const duration = parseInt(durationString);
+        return (duration >= MIN_GAME_DURATION) && (duration <= MAX_GAME_DURATION);
+    }
+    return false;
+}
+
+export function isValidWorldSeed(worldSeed: string) {
+    // TODO: implement properly
+    return true ? worldSeed.length == 0 || isNumber(worldSeed) : false;
+}
+
+// https://stackoverflow.com/questions/23437476/in-typescript-how-to-check-if-a-string-is-numeric
+export function isNumber(value?: string | number): boolean {
+    return ((value != null)
+        && (value != '')
+        && !isNaN(Number(value.toString())));
 }
