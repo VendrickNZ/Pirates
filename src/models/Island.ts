b337f781febeb9160fb01c9035bc8da8aaad4c82
Jakib <jakibisherwood88@gmail.com>
2026-03-08 14:42:43 +1300

    Extend Island to include commodities, add assign func

--- src/models/Island.ts
+++ src/models/Island.ts
@@ -2,23 +2,72 @@ import type { Interface } from "readline";
 import type { GameState } from "../types/GameState";
 import { timeoutInSeconds } from "../utils/TextUtils";
 import type Player from "./Player";
-import { GameItems } from "../types/Item";
+import type { ItemType } from "../types/Item";
 
 type IslandNames = 'Barataria Bay' | 'Port Royal' | 'Tortuga' | 'Prince Edward Island'
-export type Island = Record<'name', IslandNames>;
+type IslandCommoditiesTable = Record<ItemType, number>[];
+
+export type Island = {
+    name: IslandNames,
+    commodities: IslandCommoditiesTable
+}
 
 const islands: Island[] = [
     {
-        "name": "Barataria Bay"
+        name: "Barataria Bay",
+        commodities: [
+            {
+                "Food": 1.00,
+                "Weapon": 1.00,
+                "Luxury": 1.00,
+                "Natural Resource": 1.00,
+                "Alcohol": 1.00,
+                "Common": 1.00,
+                "Medicine": 1.00
+            }
+        ]
     },
     {
-        "name": "Port Royal"
+        name: "Port Royal",
+        commodities: [
+            {
+                "Food": 1.00,
+                "Weapon": 1.00,
+                "Luxury": 1.00,
+                "Natural Resource": 1.00,
+                "Alcohol": 1.00,
+                "Common": 1.00,
+                "Medicine": 1.00
+            }
+        ]
     },
     {
-        "name": "Tortuga"
+        name: "Tortuga",
+        commodities: [
+            {
+                "Food": 1.00,
+                "Weapon": 1.00,
+                "Luxury": 1.00,
+                "Natural Resource": 1.00,
+                "Alcohol": 1.00,
+                "Common": 1.00,
+                "Medicine": 1.00
+            }
+        ]
     },
     {
-        "name": "Prince Edward Island"
+        name: "Prince Edward Island",
+        commodities: [
+            {
+                "Food": 1.00,
+                "Weapon": 1.00,
+                "Luxury": 1.00,
+                "Natural Resource": 1.00,
+                "Alcohol": 1.00,
+                "Common": 1.00,
+                "Medicine": 1.00
+            }
+        ]
     }
 ]
 
@@ -36,11 +85,19 @@ function assignIslandIds(): IslandRegistry {
     return registry;
 }
 
-// function assignRoutes(islandsById: IslandRegistry): IslandRoutes {
-//     for (const island of islandsById) {
+function assignAllRoutes(islandsById: IslandRegistry): IslandRoutes {
+    const islandRoutes: IslandRoutes = {};
+    for (const island of Object.entries(islandsById)) {
+        const islandId = Number(Object.keys(island));
+        islandRoutes[islandId] = assignRoutes(island);
+    }
 
-//     }
-// }
+    return islandRoutes;
+}
+
+function assignRoutes(island: [string, Island]): Route[] {
+    return [];
+}
 
 class WorldGraph {
     private _islandsById: IslandRegistry;
@@ -48,24 +105,24 @@ class WorldGraph {
 
     constructor() {
         this._islandsById = assignIslandIds();
-        this._routesFrom = {
-            1: [{
-                to: 2,
-                distanceKm: 1,
-                travelDays: 1,
-                encounterTable: {
-                    1: 'Pirates',
-                    99: 'Lightning Strikes'
-                }
-            }]
-        }
+        // this._routesFrom = {
+        //     1: [{
+        //         to: 2,
+        //         distanceKm: 1,
+        //         travelDays: 1,
+        //         encounterTable: {
+        //             1: 'Pirates',
+        //             99: 'Lightning Strikes'
+        //         }
+        //     }]
+        // }
+        this._routesFrom = assignAllRoutes(this.islandsById)
     }
 
     get islandsById() {
         return this._islandsById;
     }
 }
-
 
 type Route = {
     to: number,
@@ -89,11 +146,8 @@ export function getStartingIsland(): Island {
 
 export async function visitDocks(player: Player, rl: Interface): Promise<GameState> {
     const x = new WorldGraph();
-    x.islandsById;
+    console.log(x.islandsById);
     printAvailableRoutes();
-    for (const item of GameItems) {
-        console.log(item.name, item.units);
-    }
     await timeoutInSeconds(3);
     return 'At Island'
 }
