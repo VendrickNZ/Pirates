c48431eb8672676bc27eda78ef94d90f4d16af99
Jakib <jakibisherwood88@gmail.com>
2026-04-12 13:19:47 +1200

    Remove GUI - can add later

--- src/game/runGUI.ts
+++
@@ -1,3 +0,0 @@
-export default function runGUI() {
-    console.log('I am now running the GUI!');
-}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
 import runCLI from "./game/runCLI";
 import { runDev } from "./game/runDev";
-import runGUI from "./game/runGUI";
 
 switch (process.argv[2]) {
   case 'nogui':
     runCLI();
-    break;
-  case 'gui':
-    runGUI();
     break;
   default:
     runDev();
